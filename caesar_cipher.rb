14b077bdd9fd90ddf7ea47319d59dc980b1c24cc
kulewers <kule.wers@gmail.com>
2023-02-02 20:03:47 +0200

    Initial commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+require 'pry-byebug'
+
+def caesar_cipher(string, shift)
+  ciphered_string = string.split('').map do |letter|
+    letter_code = letter.ord
+    if letter_code >= 65 && letter_code <= 90
+      letter_code -= 65
+      (((letter_code + shift) % 26) + 65).chr
+    elsif letter_code >= 97 && letter_code <= 122
+      letter_code -= 97
+      (((letter_code + shift) % 26) + 97).chr
+    else
+      letter_code.chr
+    end
+  end
+  p ciphered_string.join('')
+end
+
+caesar_cipher("What a string!", -5)
